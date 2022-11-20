08fc985904306ab310c8e12bd97d45df82cdbb18
Xxploiter <dipankarthedestroyer@gmail.com>
2022-11-20 19:48:48 +0530

    Fixed Admin UI decomposed the index to header sidebar footer added some css modified paddings, margin etc

---
+++ app/views/admin/footer.php
@@ -0,0 +1,43 @@
+
+        <!--**********************************
+            Footer start
+        ***********************************-->
+        <div class="footer">
+            <div class="copyright">
+                <p>Copyright © Designed &amp; Developed by <a href="../index.htm" target="_blank">Crio</a> 2022</p>
+            </div>
+        </div>
+        <!--**********************************
+            Footer end
+        ***********************************-->
+    </div>
+    <!--**********************************
+        Main wrapper end
+    ***********************************-->
+
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <!-- Required vendors -->
+    <!-- <script data-cfasync="false" src="../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script> -->
+	<script src="<?php echo ASSETS . ADMIN_THEME  ?>vendor/global/global.min.js"></script>
+	<script src="<?php echo ASSETS . ADMIN_THEME  ?>vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+	<script src="<?php echo ASSETS . ADMIN_THEME  ?>vendor/chart.js/Chart.bundle.min.js"></script>
+	
+	<!-- Chart piety plugin files -->
+    <script src="<?php echo ASSETS . ADMIN_THEME  ?>vendor/peity/jquery.peity.min.js"></script>
+	
+	<!-- Apex Chart -->
+	<script src="<?php echo ASSETS . ADMIN_THEME  ?>vendor/apexchart/apexchart.js"></script>
+	
+	<!-- Dashboard 1 -->
+	<script src="<?php echo ASSETS . ADMIN_THEME  ?>js/dashboard/dashboard-1.js"></script>
+	
+	<script src="<?php echo ASSETS . ADMIN_THEME  ?>vendor/owl-carousel/owl.carousel.js"></script>
+    <script src="<?php echo ASSETS . ADMIN_THEME  ?>js/custom.min.js"></script>
+	<script src="<?php echo ASSETS . ADMIN_THEME  ?>js/deznav-init.js"></script>
+    <script src="<?php echo ASSETS . ADMIN_THEME  ?>js/demo.js"></script>
+    <script src="<?php echo ASSETS . ADMIN_THEME  ?>js/styleSwitcher.js"></script>
+
+</body>
+</html>
